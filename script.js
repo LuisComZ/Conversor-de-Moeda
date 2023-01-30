f539204595275988a8061e29d25ac7461346dabd
Luiz <luizmay2@gmail.com>
2023-01-30 20:00:27 -0300

    Just learning a little more of JavaScript

---
+++ script.js
@@ -0,0 +1,17 @@
+var valorEmDolar = 64
+var cotacaoDoDolar = 5.32
+
+var valorEmReal = valorEmDolar * cotacaoDoDolar
+valorEmReal = valorEmReal.toFixed(2)
+
+var nome = "Luiz"
+
+alert("Olá, " + nome + ", o valor em Real é: " + "R$ " + valorEmReal)
+
+//var valorEmKilometro = 2
+//var medidaAnoLuz = 9.46
+
+//var valorEmAnoLuz = valorEmKilometro * medidaAnoLuz
+//valorEmAnoLuz = valorEmAnoLuz.toFixed(1)
+
+//alert("O valor é: " + valorEmAnoLuz + " trilhões de Anos Luz.")
